0344984de0db0324fdca6d0927eee96497999f9f
Vlad <putyn@ymail.com>
2017-08-19 10:37:07 +0300

    preparing to merge #2

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -6,14 +6,19 @@ var cleancss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var gzip = require('gulp-gzip');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
 var del = require('del');
 
-/* Clean destination folder */
-gulp.task('clean', function() {
+/* Clean dist/build folder */
+gulp.task('clean:all', function() {
     return del(['dist/*']);
 });
+gulp.task('clean:build', function() {
+    return del(['dist/build/*']);
+});
 
-/* Copy static file */
+/* copy static file based on variant */
 gulp.task('files_nixie', function() {
   return gulp.src([
     'src/**/glyphicons.woff2',
@@ -31,31 +36,33 @@ gulp.task('files_seven', function() {
   ])
   .pipe(gulp.dest('dist'));
 });
-/* preprocess file, inline it, mini - do the same for both variants*/
+/* Preprocess file, do the same for both variants, merge css and js */
 gulp.task('preproc_seven', function() {
   return gulp.src(['src/index.html'])
   .pipe(preprocess({context: { VARIANT: 'seven', VARIANT_NAME: 'Big Seven', VARIANT_navbar: 'inverse'}}))
-  .pipe(inline({
-    base: 'src/',
-    js: uglify,
-    css: cleancss,
-  }))
-  .pipe(htmlmin({
-    collapseWhitespace: true,
-    removeComments: true,
-    minifyCSS: true,
-    minifyJS: true
-  }))
+  .pipe(useref())
+  .pipe(gulpif('*.css', cleancss()))
+  .pipe(gulpif('*.js', uglify()))
   .pipe(gulp.dest('dist'))
 });
 
 gulp.task('preproc_nixie', function() {
   return gulp.src(['src/index.html'])
   .pipe(preprocess({context: { VARIANT: 'nixie', VARIANT_NAME:'NAKED Nixie', VARIANT_navbar: 'default'}}))
+  .pipe(useref())
+  .pipe(gulpif('*.css', cleancss()))
+  .pipe(gulpif('*.js', uglify()))
+  .pipe(gulp.dest('dist'))
+});
+
+/* build file system for ESP8266, combine all files into one big file and gzip it */
+gulp.task('gzip', function() {
+  return gulp.src(['dist/index.html'])
   .pipe(inline({
-    base: 'src/',
+    base: 'dist/',
     js: uglify,
     css: cleancss,
+    disabledTypes: ['svg', 'img']
   }))
   .pipe(htmlmin({
     collapseWhitespace: true,
@@ -63,26 +70,33 @@ gulp.task('preproc_nixie', function() {
     minifyCSS: true,
     minifyJS: true
   }))
-  .pipe(gulp.dest('dist'))
-});
-/* GZIPPPP */
-gulp.task('gzip', function() {
-  return gulp.src(['dist/index.html'])
   .pipe(gzip())
-  .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest('dist/build'))
 });
 
-/* Some tasks */
-gulp.task('seven', function (e) {
-   runSequence('clean', 'files_seven', 'preproc_seven', e);
+gulp.task('files_fs', function() {
+  return gulp.src([
+    'dist/**/*.{ico,woff2}'
+  ])
+  .pipe(gulp.dest('dist/build'));
 });
-gulp.task('seven_fs', function (e) {
-   runSequence('clean', 'files_seven', 'preproc_seven', 'gzip', e);
+
+/* tasks */
+gulp.task('seven', function (e) {
+   runSequence('clean:all', 'files_seven', 'preproc_seven', e);
 });
 gulp.task('nixie', function (e) {
-  runSequence ('clean', 'files_nixie', 'preproc_nixie', 'gzip', e);
+  runSequence ('clean:all', 'files_nixie', 'preproc_nixie', e);
 });
-gulp.task('nixie_fs', function (e) {
-  runSequence ('clean', 'files_nixie', 'preproc_nixie', 'gzip', e);
+gulp.task('build_fs', function (e) {
+   runSequence('clean:build', 'gzip', 'files_fs', e);
+});
+/* watch files for faster developemnt */
+gulp.task('seven_watch', function() {
+  gulp.watch('src/index.html', ['seven']);
+});
+
+gulp.task('nixie_watch', function() {
+  gulp.watch('src/index.html', ['nixie']);
 });
 
